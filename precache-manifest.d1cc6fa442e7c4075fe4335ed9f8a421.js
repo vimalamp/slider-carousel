c1902b469ca60fa22385cd3f91ada20dd5b3843f
Linux User <user@null.my.domain>
2020-07-11 21:51:22 -1000

    Updates

---
+++ precache-manifest.d1cc6fa442e7c4075fe4335ed9f8a421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27003c74dda707b108d985989051df9c",
+    "url": "/slider-carousel/index.html"
+  },
+  {
+    "revision": "1afe1d79986019628c18",
+    "url": "/slider-carousel/static/css/main.7b1911ee.chunk.css"
+  },
+  {
+    "revision": "1e6f03e3576ce4c336ca",
+    "url": "/slider-carousel/static/js/2.6fffb642.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slider-carousel/static/js/2.6fffb642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1afe1d79986019628c18",
+    "url": "/slider-carousel/static/js/main.20e1c4a7.chunk.js"
+  },
+  {
+    "revision": "9fc6e075e98bf0606f47",
+    "url": "/slider-carousel/static/js/runtime-main.5ec6db51.js"
+  }
+]);
